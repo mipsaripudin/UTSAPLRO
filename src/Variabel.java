1dc82b88e2560085b3cd5dd57021bf1d67576a53
mipsaripudin <mip.saripudin@unida.ac.id>
2021-11-20 09:43:26 +0700

    Soal No 5. Variabel.Java
    Ini adalah jawaban soal nomor 5. Membuat Varibel pada Java

---
+++ src/Variabel.java
@@ -0,0 +1,11 @@
+
+public class Variabel {
+	public static void main(String[] args) {
+		String Salam = "Assalamualaikum. Wr. Wb";
+		String Nama = "Mip Saripudin";
+		int Tahun = 1995;
+		System.out.println(Salam);
+		System.out.println("Nama Saya : " + Nama);
+		System.out.println("Saya Lahir di Tahun : " + Tahun);
+	}
+}
